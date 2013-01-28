89ee74740e577bd2e8f38bcc77feb866fcf513a6
Richard Millet <milletdesigns@me.com>
2013-01-28 00:05:11 -0800

    Added circle view and dealt with funky 3.5 in vs. 4.0 in iPhone layout issue.

---
+++ Happiness/SCSCircleView.h
@@ -0,0 +1,13 @@
+//
+//  SCSCircleView.h
+//  Happiness
+//
+//  Created by Richard E Millet on 1/27/13.
+//  Copyright (c) 2013 Richard Millet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSCircleView : UIView
+
+@end
---
+++ Happiness/SCSCircleView.m
@@ -0,0 +1,34 @@
+//
+//  SCSCircleView.m
+//  Happiness
+//
+//  Created by Richard E Millet on 1/27/13.
+//  Copyright (c) 2013 Richard Millet. All rights reserved.
+//
+
+#import "SCSCircleView.h"
+
+@implementation SCSCircleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+ */
+- (void)drawRect:(CGRect)rect
+{
+    [[UIColor blueColor] setStroke];
+    UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:rect];
+    circlePath.lineWidth = 4;
+    [circlePath stroke];
+}
+
+@end
